16ff4afe9edc8e0988b9714e32f98e0b030e5aa2
Alexander LAM <alam23@cs-1.CS.McGill.CA>
2019-09-30 12:58:34 -0400

    work in progress, getline()"

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+char* get_a_line(){
+    char* line = NULL;
+    size_t bufferSize = 0;
+    ssize_t nread;
+
+    // Getline allocates a buffer an assigns it to line if line is null, *bufferSize is updated with size of buffer. 
+    // it is the responsibility of the caller to free this buffer in this case. 
+    nread = getline(&line, &bufferSize, stdin);
+
+    return line;
+}
+
+void my_system(char* line){
+    printf("From my_system: %s", line);
+}
+
+int main(int argc, char* arv[] ) {
+    char* line;
+    while(1){
+        line = get_a_line();
+        if(strlen(line) > 1){
+            my_system(line);
+        }
+    }
     return 0;
 }
